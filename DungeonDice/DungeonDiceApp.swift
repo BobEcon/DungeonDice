4ddbf7971c425e5114c81791079e603ab1a08577
Bob Beachill <bbeachill@icloud.com>
2025-03-17 22:48:51 +0000

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Robert Beachill on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
